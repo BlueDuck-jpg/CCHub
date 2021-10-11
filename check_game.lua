8779b387a21bf60f365da6aa5e6950ec5413fea6
BlueDuck-jpg <73210133+BlueDuck-jpg@users.noreply.github.com>
2021-10-11 19:16:42 +0800

    Create check_game.lua

---
+++ check_game.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 1962086868 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/toh.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/univ.lua"))()
+end
