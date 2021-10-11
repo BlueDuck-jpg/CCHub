7e7d0aa554788253e48f723e0ca1e37d6a1444b3
BlueDuck-jpg <73210133+BlueDuck-jpg@users.noreply.github.com>
2021-10-11 21:27:42 +0800

    Update check_game.lua

--- check_game.lua
+++ check_game.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 1962086868 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/toh.lua"))()
+elseif game.PlaceId == 6067050638 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/tyt.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/univ.lua"))()
 end
