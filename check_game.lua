f12809ae715dab3e15e481b946de90376cca7105
BlueDuck-jpg <73210133+BlueDuck-jpg@users.noreply.github.com>
2021-10-12 09:01:41 +0800

    Update check_game.lua

--- check_game.lua
+++ check_game.lua
@@ -1,7 +1,21 @@
-if game.PlaceId == 1962086868 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/toh.lua"))()
-elseif game.PlaceId == 6067050638 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/tyt.lua"))()
+local key;
+key = game.HttpService:JSONDecode(readfile("cchub.json"))
+local requests = request or syn.request
+local check_key = requests({
+    Url="https://cchub.smktd.repl.co/findkey?key=" .. key.cchubkey,
+    Method="GET",
+    Headers=headers
+})
+local check_blacklist = game.HttpService:JSONEncode(check_key.Body):split('"')[16]:split(":")[2]:split("}")[1]
+
+if check_blacklist ~= "true" then
+    if game.PlaceId == 1962086868 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/toh.lua"))()
+    elseif game.PlaceId == 6067050638 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/tyt.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/univ.lua"))()
+    end
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlueDuck-jpg/CCHub/main/univ.lua"))()
+    game.Players.LocalPlayer:Kick("Your account have been blacklisted!")
 end
